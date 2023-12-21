18a60e8708ef2ad4b7ceddc6a1474ff7120fa4bc
Mirkocole <97846706+Mirkocole@users.noreply.github.com>
2023-12-21 18:58:44 +0100

    consegna esercizio

---
+++ script.js
@@ -0,0 +1,7 @@
+const albums = [
+    {image: 'assets/',name:'', artist: '', genre: '', duration: 23}
+]
+
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log('Content Caricato');
+});
